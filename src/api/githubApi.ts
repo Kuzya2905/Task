91f153fc1fbafe147dce455195ded3047e43dcd2
Константин Кузьмин <kuzmin2905@gmail.com>
2024-08-19 10:51:07 +0300

    fix githubApi

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -1,14 +1,10 @@
 import axios from "axios";
 
 const GITHUB_API_URL = "https://api.github.com"; // Константа для базового URL GitHub API
-const GITHUB_API_TOKEN = "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"; // Личный токен GitHub
 
 // Экземпляр axios для работы с GitHub API
 export const githubApi = axios.create({
   baseURL: GITHUB_API_URL,
-  headers: {
-    Authorization: `Bearer ${GITHUB_API_TOKEN}`,
-  },
 });
 
 // Endpoints для GitHub API
